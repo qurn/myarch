48d56145c63f02ce3e38fbf9570e0bfa98edba5d
qurn <32361644+qurn@users.noreply.github.com>
2018-11-07 21:58:00 +0100

    Create arch_2.sh

---
+++ arch_2.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+printf \
+"de_DE.UTF-8 UTF-8
+de_DE ISO-8859-1
+de_DE@euro ISO-8859-15" \
+> /etc/locale.gen
+
+echo LANG=de_DE.UTF-8 > /etc/locale.conf
+locale-gen
+
+printf \
+"echo KEYMAP=de-latin1
+echo FONT=lat9w-16" \
+> /etc/vconsole.conf
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+echo $HOSTNAME > /etc/hostname
+echo "127.0.0.1	$HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+
+#wired
+systemctl enable dhcpcd.service
+
+pacman -Syu
+pkgfile -u
+
+###############
+#bootctl
+###############
+
+bootctl install
+
+cp /etc/mkinitcpio.conf /etc/mkinitcpio.confBAK
+
+printf /
+"MODULES=()
+BINARIES=()
+FILES=()
+HOOKS=(base udev autodetect modconf block filesystems keyboard encrypt fsck)" \ 
+> /etc/mkinitcpio.conf
+
+printf \
+"timeout 0
+default arch" \
+> /boot/loader/loader.conf
+
+CRYPTUUID="$(blkid $DRIVE\2 | sed -r -n 's:.*\ UUID="([a-f0-9-]*).*:\1:p')"
+
+printf \
+"title Archlinux
+linux /vmlinuz-linux
+initrd /intel-ucode.img
+initrd /initramfs-linux.img
+options cryptdevice=UUID=$CRYPTUUID:cryptroot root=/dev/mapper/cryptroot quiet rw" \
+> /boot/loader/entries/arch.conf
+
+mkinitcpio -p linux
+
+passwd
+useradd -m -g users -G wheel,audio,video -s /bin/bash $USERNAME
+passwd $USERNAME
+visudo #wheel
+
+gsettings set org.nemo.desktop show-desktop-icons false
+
+################
+#Sound
+################
+printf \
+"snd-seq-oss
+snd-pcm-oss
+snd-mixer-oss" \
+> /etc/modules-load.d/alsaoss.conf
+
+exit
